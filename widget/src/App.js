a0f9a10bfe7f5dd01420ebbb685dae720c3f13f9
Matthew Quint <mattquint111@gmail.com>
2020-12-20 12:39:58 -0600

    widget-navigation completed

--- widget/src/App.js
+++ widget/src/App.js
@@ -3,7 +3,20 @@ import "./App.css";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
-import Translate from './components/Translate'
+import Translate from "./components/Translate";
+import Route from './components/Route'
+import Header from './components/Header'
+
+const items = [
+    {
+        title: "What is React?",
+        content: "React is a front end javascript framework",
+    },
+    {
+        title: "Why use React?",
+        content: "React is a favorite JS library among engineers.",
+    },
+];
 
 const options = [
     {
@@ -20,11 +33,31 @@ const options = [
     },
 ];
 
+
 export default () => {
+    const [selected, setSelected] = useState(options[0])
 
     return (
         <div>
-            <Translate />
+            <Header />
+            <Route path="/" >
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown 
+                    label="Select a color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
+
         </div>
     );
 };
