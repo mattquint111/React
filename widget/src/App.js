3bc8592ea903dd8856856795ba37bab694912b7f
Matthew Quint <mattquint111@gmail.com>
2020-12-20 09:19:05 -0600

    widget - Dropdown - completed

--- widget/src/App.js
+++ widget/src/App.js
@@ -1,27 +1,39 @@
-import React from "react";
-import './App.css'
-import Accordion from './components/Accordion';
-import Search from './components/Search'
+import React, { useState } from "react";
+import "./App.css";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+
+const options = [
+    {
+        label: "Red",
+        value: "red",
+    },
+    {
+        label: "Green",
+        value: "green",
+    },
+    {
+        label: "Blue",
+        value: "blue",
+    },
+];
 
 export default () => {
-    const items = [
-        {
-            title: "What is React?",
-            content: "React is a front end framework",
-        },
-        {
-            title: "Why use React?",
-            content: "React is a favorite front end library among engineers",
-        },
-        {
-            title: "How do you use React?",
-            content: "You use React by creating components",
-        },
-    ];
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true)
 
     return (
         <div>
-            <Search />
+        <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+        {showDropdown ?
+            <Dropdown
+                selected={selected}
+                onSelectedChange={setSelected}
+                options={options}
+            /> : null
+        }
+            
         </div>
     );
 };
