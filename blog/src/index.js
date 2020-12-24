7ccf7a181d0fcd06683b327f96d731e01ab1c4fa
Matthew Quint <mattquint111@gmail.com>
2020-12-24 16:46:35 -0600

    redux-thunk practice

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk'
 
 import "./index.css";
 import App from "./components/App";
@@ -9,7 +10,7 @@ import reducers from "./reducers";
 
 const store = createStore(
     reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk),
 );
 
 ReactDOM.render(
