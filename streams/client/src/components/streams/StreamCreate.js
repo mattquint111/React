b715181bbe86e8e5bb88ab5156607a6b27fa01cc
Matthew Quint <mattquint111@gmail.com>
2020-12-27 15:09:51 -0600

    redux-form

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends React.Component {
+
     renderInput({ input }) {
         return <input {...input} />
+
     }
 
     render() {
+
         return (
+            
             <form>
                 <Field name="title" component={this.renderInput} />
                 <Field name="description" component={this.renderInput} />
