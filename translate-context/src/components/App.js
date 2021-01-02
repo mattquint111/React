d43af324b8f8e5bc2b9b3169f3c633debd7f78b6
Matthew Quint <mattquint111@gmail.com>
2021-01-02 08:18:37 -0600

    context provider

---
+++ translate-context/src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import 'semantic-ui-css/semantic.min.css'
+import UserCreate from './UserCreate'
+import LanguageContext from '../contexts/LanguageContext'
+import ColorContext from '../contexts/ColorContext'
+
+class App extends Component {
+    state = { language: 'english' }
+
+    onLanguageChange = language => {
+        this.setState({ language })
+    }
+
+    render() {
+    return(
+        <div className="ui container">
+            <div>
+                Select a language: 
+                <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+                <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+            </div>
+            <LanguageContext.Provider value={this.state.language} >
+                <ColorContext.Provider value='primary' >
+                    <UserCreate />
+                </ColorContext.Provider>
+            </LanguageContext.Provider>
+        </div>
+    )
+    }
+}
+
+export default App
